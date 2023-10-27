19205003054f3b7dbd240446e71ac2497aa8972d
laylaksn <laylasadeghi@gmail.com>
2023-10-27 14:23:40 +0100

    lightbox

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const projectTiles = document.querySelectorAll(".project-tile");
-    const lightbox = document.querySelector(".lightbox");
-    const lightboxContent = document.querySelector(".lightbox-content");
-  
-    projectTiles.forEach(tile => {
-      tile.addEventListener("click", function() {
-        lightbox.style.display = "block";
-        // Add project-specific content to lightboxContent here
-        lightboxContent.innerHTML = "Additional information about the project";
-      });
-    });
-  
-    lightbox.addEventListener("click", function(e) {
-      if (e.target === lightbox) {
-        lightbox.style.display = "none";
-      }
-    });
+    // JavaScript to handle the lightbox
+
+// Get the project tiles and lightbox elements
+const projectTiles = document.querySelectorAll('.project-tile');
+const lightbox = document.getElementById('project-lightbox');
+const closeLightbox = document.getElementById('close-lightbox');
+
+// Function to open the lightbox
+function openLightbox(project) {
+  lightbox.style.display = 'block';
+}
+
+// Function to close the lightbox
+function closeLightbox() {
+  lightbox.style.display = 'none';
+}
+
+// Event listeners for opening and closing the lightbox
+projectTiles.forEach((tile, index) => {
+  tile.addEventListener('click', () => {
+    openLightbox(index);
   });
-  
+});
+
+closeLightbox.addEventListener('click', () => {
+  closeLightbox();
+});
+
+// Close the lightbox when clicking outside the content
+window.addEventListener('click', (event) => {
+  if (event.target === lightbox) {
+    closeLightbox();
+  }
+});
