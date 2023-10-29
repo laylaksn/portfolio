9f7e11acb073fb1b3bf57802923c11518885c49e
laylaksn <laylasadeghi@gmail.com>
2023-10-29 09:09:35 +0000

    sa

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // JavaScript to handle the lightbox
-
-// Get the project tiles and lightbox elements
-const projectTiles = document.querySelectorAll('.project-tile');
-const lightbox = document.getElementById('project-lightbox');
-const closeLightbox = document.getElementById('close-lightbox');
-
-// Function to open the lightbox
-function openLightbox(project) {
-  lightbox.style.display = 'block';
-}
-
-// Function to close the lightbox
-function closeLightbox() {
-  lightbox.style.display = 'none';
-}
-
-// Event listeners for opening and closing the lightbox
-projectTiles.forEach((tile, index) => {
-  tile.addEventListener('click', () => {
-    openLightbox(index);
+document.addEventListener("DOMContentLoaded", function () {
+    // Your other JavaScript code here
+  
+    // Function to open the lightbox for a specific project
+    function openLightbox(lightboxId) {
+      var lightbox = document.getElementById(lightboxId);
+      lightbox.style.display = "block";
+    }
+  
+    // Function to close the lightbox
+    function closeLightbox(lightboxId) {
+      var lightbox = document.getElementById(lightboxId);
+      lightbox.style.display = "none";
+    }
+  
+    // Attach click event listeners to project tiles
+    document.querySelector(".project-tile:nth-child(1)").addEventListener("click", function () {
+      openLightbox("project1-lightbox");
+    });
+  
+    document.querySelector(".project-tile:nth-child(2)").addEventListener("click", function () {
+      openLightbox("project2-lightbox");
+    });
+  
+    document.querySelector(".project-tile:nth-child(3)").addEventListener("click", function () {
+      openLightbox("project3-lightbox");
+    });
   });
-});
-
-closeLightbox.addEventListener('click', () => {
-  closeLightbox();
-});
-
-// Close the lightbox when clicking outside the content
-window.addEventListener('click', (event) => {
-  if (event.target === lightbox) {
-    closeLightbox();
-  }
-});
+  
